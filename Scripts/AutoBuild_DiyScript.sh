806d11fbcb041f4775b551cbc661a320f1306a9d
Across <jsnjycwyz@gmail.com>
2025-06-07 19:02:20 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -150,8 +150,8 @@ EOF
 			case "${CONFIG_FILE}" in
 			x86_64)
 				# sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${FEEDS_PKG}/ttyd/files/ttyd.config
-				AddPackage passwall xiaorouji openwrt-passwall main
-				# AddPackage passwall xiaorouji openwrt-passwall2 main
+				#AddPackage passwall xiaorouji openwrt-passwall main
+				AddPackage passwall xiaorouji openwrt-passwall2 main
 				rm -r ${FEEDS_LUCI}/luci-app-passwall
 				AddPackage other WROIATE luci-app-socat main
     			#rm -r ${FEEDS_LUCI}/luci-app-socat
